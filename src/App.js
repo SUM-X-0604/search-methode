8bb0a993378114a8805470f4ce20af3afc999bdc
Sumit Verma <sumeet0604.sv@gmail.com>
2022-06-07 15:08:03 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Users } from './Users'
+import './App.css'
+import Table from './Table';
 
 function App() {
+  const [query, setQuery] = useState('');
+
+  const keys = ['first_name', "last_name", "email", 'gender']
+
+  const search = (data) => {
+    return data.filter((item) => (
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='app'>
+        <input
+          type="text"
+          placeholder='search here...'
+          className='search'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {/* first methode start */}
+        {/* <ul className='list'>
+          {Users.filter((user) => user.first_name.toLowerCase().includes(query)).map((user) => (
+            <li key={user.id} className='listItem'>
+              {user.first_name}
+            </li>
+          ))}
+        </ul> */}
+        {/* first methode end */}
+
+        {/* second methode start(Multiple search) */}
+
+        <Table data={search(Users)} />
+
+        {/* second methode end */}
+
+      </div>
+    </>
   );
 }
 
